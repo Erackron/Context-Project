67e432ada3334427e006b56dc9e8dedec465a521
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2015-05-01 10:20:55 +0200

    Add configuration to make sure pmd and findbugs are run.

--- desktop/src/main/java/nl/tudelft/contextproject/java/MainDesktop.java
+++ desktop/src/main/java/nl/tudelft/contextproject/java/MainDesktop.java
@@ -6,8 +6,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import nl.tudelft.contextproject.core.Main;
 
 public class MainDesktop {
-	public static void main (String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
-	}
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Main(), config);
+    }
 }
