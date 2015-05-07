66b6253ded219cc8d902681de7bb50ef4c3369ac
Ike Rijsdijk <ike14pp@hotmail.com>
2015-05-07 21:28:18 +0200

    Removing code from the Main to make a merge possible

--- core/src/main/java/nl/tudelft/contextproject/core/input/KeyboardInputProcessor.java
+++ core/src/main/java/nl/tudelft/contextproject/core/input/KeyboardInputProcessor.java
@@ -6,48 +6,56 @@ import com.badlogic.gdx.math.Vector2;
 import nl.tudelft.contextproject.core.entities.Player;
 import nl.tudelft.contextproject.core.positioning.Coordinate;
 
+import java.util.HashMap;
+
 /**
  * Created by Ike on 6-5-2015.
  */
 public class KeyboardInputProcessor extends InputAdapter{
 
-    public static boolean UP;
-    public static boolean DOWN;
-    public static boolean LEFT;
-    public static boolean RIGHT;
+    public static double PIXELSPERUPDATE = 1.0/25.0;
+    public static double ANGLE = 9.0/10.0;
 
-    public static boolean DRAWCLOCKWISE;
-    public static boolean DRAWCOUNTERCLOCKWISE;
-    public static boolean DRAWING;
-
+    private HashMap<Integer, Boolean> keys;
     private Player player;
+
     private Vector2 start;
     private Vector2 end;
     private Vector2 center;
 
     public KeyboardInputProcessor() {
         player = new Player();
+        keys = new HashMap<Integer, Boolean>();
+
+        keys.put(Input.Keys.W, false);
+        keys.put(Input.Keys.S, false);
+        keys.put(Input.Keys.A, false);
+        keys.put(Input.Keys.D, false);
+        keys.put(Input.Keys.UP, false);
+        keys.put(Input.Keys.DOWN, false);
+        keys.put(Input.Keys.SPACE, false);
     }
 
     public void update(float dt) {
-        if (UP) {
-            player.getPosition().add(0, 2 * dt / 50);
-            player.getBrushPosition().add(0, 2 * dt / 50);
+        if (isPressed(Input.Keys.W)) {
+            player.getPosition().add(0, (float) PIXELSPERUPDATE * dt);
+            player.getBrushPosition().add(0, (float) PIXELSPERUPDATE * dt);
         }
-        if (DOWN) {
-            player.getPosition().add(0, -2 * dt / 50);
-            player.getBrushPosition().add(0, -2 * dt / 50);
+        if (isPressed(Input.Keys.S)) {
+            player.getPosition().add(0, (float) -PIXELSPERUPDATE * dt);
+            player.getBrushPosition().add(0, (float)- PIXELSPERUPDATE * dt);
         }
-        if (LEFT) {
-            player.getPosition().add(-2 * dt / 50, 0);
-            player.getBrushPosition().add(-2 * dt / 50, 0);
+        if (isPressed(Input.Keys.A)) {
+            player.getPosition().add((float) -PIXELSPERUPDATE * dt, 0);
+            player.getBrushPosition().add((float) -PIXELSPERUPDATE * dt, 0);
         }
-        if (RIGHT) {
-            player.getPosition().add(2 * dt / 50, 0);
-            player.getBrushPosition().add(2 * dt / 50, 0);
+        if (isPressed(Input.Keys.D)) {
+            player.getPosition().add((float) PIXELSPERUPDATE * dt, 0);
+            player.getBrushPosition().add((float) PIXELSPERUPDATE * dt, 0);
         }
-        if (DRAWCLOCKWISE) {
-            float angle = 90 * dt / 100;
+
+        if (isPressed(Input.Keys.DOWN)) {
+            double angle = ANGLE * dt;
 
             float newX = (float) Math.cos(angle) * player.getRadius() + player.getPosition().x;
             float newY = (float) Math.sin(angle) * player.getRadius() + player.getPosition().y;
@@ -55,8 +63,8 @@ public class KeyboardInputProcessor extends InputAdapter{
             player.getBrushPosition().set(newX, newY);
         }
 
-        if (DRAWCOUNTERCLOCKWISE) {
-            float angle = -90 * dt/ 100;
+        if (isPressed(Input.Keys.UP)) {
+            double angle = -ANGLE * dt;
 
             float newX = (float) Math.cos(angle) * player.getRadius() + player.getPosition().x;
             float newY = (float) Math.sin(angle) * player.getRadius() + player.getPosition().y;
@@ -70,54 +78,37 @@ public class KeyboardInputProcessor extends InputAdapter{
     @Override
     public boolean keyDown(int i) {
 
-        if (i == Input.Keys.W) {
-            UP = true;
-        } else if (i == Input.Keys.S) {
-            DOWN = true;
-        } else if (i == Input.Keys.A) {
-            LEFT = true;
-        } else if (i == Input.Keys.D) {
-            RIGHT = true;
-        } else if (i == Input.Keys.DOWN) {
-            DRAWCLOCKWISE = true;
-        } else if (i == Input.Keys.UP) {
-            DRAWCOUNTERCLOCKWISE = true;
-        } else if (i == Input.Keys.SPACE) {
-            DRAWING = true;
+        if (i == Input.Keys.SPACE) {
             start = player.getBrushPosition();
+            keys.put(i, true);
+        } else if (keys.containsKey(i)) {
+            keys.put(i, true);
         }
 
         return true;
-
     }
 
     @Override
     public boolean keyUp(int i) {
-        if (i == Input.Keys.W) {
-            UP = false;
-        } else if (i == Input.Keys.S) {
-            DOWN = false;
-        } else if (i == Input.Keys.A) {
-            LEFT = false;
-        } else if (i == Input.Keys.D) {
-            RIGHT = false;
-        } else if (i == Input.Keys.DOWN) {
-            DRAWCLOCKWISE = false;
-        } else if (i == Input.Keys.UP) {
-            DRAWCOUNTERCLOCKWISE = false;
-        } else if (i == Input.Keys.SPACE) {
-            DRAWING = false;
-            end = player.getBrushPosition();
 
+        if (i == Input.Keys.SPACE) {
+            end = player.getBrushPosition();
+            keys.put(i, false);
             //send PlayerMovement object here;
+
+        } else if (keys.containsKey(i)) {
+            keys.put(i, false);
         }
 
         return true;
     }
 
-    @Override
-    public boolean keyTyped(char c) {
-        return false;
+    public boolean isPressed(int key) {
+        return keys.get(key);
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 
 }
