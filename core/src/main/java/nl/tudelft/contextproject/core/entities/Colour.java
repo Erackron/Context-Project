50edcc9c371b9b51bba4a67f89e1097c1c2760dc
Ike Rijsdijk <ike14pp@hotmail.com>
2015-05-21 17:31:59 +0200

    Added combining methods and tests to the Colour class

--- core/src/main/java/nl/tudelft/contextproject/core/entities/Colour.java
+++ core/src/main/java/nl/tudelft/contextproject/core/entities/Colour.java
@@ -3,6 +3,10 @@ package nl.tudelft.contextproject.core.entities;
 import com.badlogic.gdx.graphics.Color;
 import lombok.Getter;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by Mitchell on 12-5-2015.
  */
@@ -17,5 +21,47 @@ public enum Colour {
 
     Colour(int r, int g, int b){
         this.color = new Color(r, g, b, 1);
+    }
+
+    public static boolean areComplementary(Collection<Colour> colours) {
+        if (colours.size() != 2) {
+            return false;
+        } else {
+            if (    colours.contains(RED) && colours.contains(GREEN) ||
+                    colours.contains(YELLOW) && colours.contains(PURPLE) ||
+                    colours.contains(BLUE) && colours.contains(ORANGE)) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+    public static Colour combine(Collection<Colour> colours){
+
+        if (colours.size() != 2) {
+            return BLACK;
+        } else {
+
+            if (colours.contains(RED) && colours.contains(BLUE)) {
+                return PURPLE;
+            } else if (colours.contains(RED) && colours.contains(YELLOW)) {
+                return ORANGE;
+            } else if (colours.contains(BLUE) && colours.contains(YELLOW)) {
+                return GREEN;
+            } else if(areComplementary(colours)) {
+                return BLACK;
+            } else {
+
+                Iterator<Colour> it = colours.iterator();
+                Colour first = it.next();
+                Colour second = it.next();
+
+                if (first == RED || first == BLUE || first == YELLOW) {
+                    return first;
+                } else {
+                    return second;
+                }
+            }
+        }
     }
 }
---
+++ core/src/main/java/nl/tudelft/contextproject/core/entities/WrongListSizeException.java
@@ -0,0 +1,12 @@
+package nl.tudelft.contextproject.core.entities;
+
+/**
+ * Created by Ike on 21-5-2015.
+ */
+public class WrongListSizeException extends Exception{
+
+    public WrongListSizeException(String message) {
+        super(message);
+    }
+
+}
