86073c210179dfa99b2244a39ef6e45c503fa6e4
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2015-05-28 12:32:46 +0200

    Add actual ImageProcessing class
    The reason this was not in the last two commits was some trouble with removing the old submodule

---
+++ image-processing/src/main/java/nl/tudelft/context/project/imageprocessing/ImageProcessing.java
@@ -0,0 +1,11 @@
+package nl.tudelft.context.project.imageprocessing;
+
+public class ImageProcessing {
+    public ImageProcessing() {
+        //TODO Initialise OpenCV stuff
+    }
+
+    public void loop() {
+        //TODO Implement actual processing loop
+    }
+}
