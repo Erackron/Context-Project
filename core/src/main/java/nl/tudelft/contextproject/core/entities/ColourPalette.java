f8f6705c7b4c3a244aae4ca6ad3388bdb89b8cd8
Mitchell <mitchell.h.lang@gmail.com>
2015-06-03 23:06:22 +0200

    forgot small thing

--- core/src/main/java/nl/tudelft/contextproject/core/entities/ColourPalette.java
+++ core/src/main/java/nl/tudelft/contextproject/core/entities/ColourPalette.java
@@ -48,7 +48,7 @@ public class ColourPalette {
         colours.add(Colour.RED);
         colours.add(Colour.BLUE);
         colours.add(Colour.YELLOW);
-        colours.add(Colour.BLACK);
+        colours.add(Colour.WHITE);
         return new ColourPalette(colours);
     }
 }
