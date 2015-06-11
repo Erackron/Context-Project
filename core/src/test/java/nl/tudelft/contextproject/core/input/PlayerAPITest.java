01980f598cb80988b590a6686ba13277e8501c74
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2015-06-11 11:46:13 +0200

    Improve PlayerAPI test coverage

--- core/src/test/java/nl/tudelft/contextproject/core/input/PlayerAPITest.java
+++ core/src/test/java/nl/tudelft/contextproject/core/input/PlayerAPITest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.contextproject.core.input;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.AtomicQueue;
 import nl.tudelft.contextproject.core.config.Constants;
 import org.junit.Before;
@@ -8,6 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
@@ -16,16 +18,38 @@ import static org.mockito.Mockito.spy;
 
 public class PlayerAPITest {
     PlayerAPI api = PlayerAPI.getPlayerApi();
+    int width = 100;
+    int height = 150;
 
     @Before
     public void setupCleanAPI() {
         api.playerQueue = new AtomicQueue<>(Constants.MOVEMENT_QUEUE_CAPACITY);
+        api.cameraInputSize = null;
     }
 
     @Test
     public void testSingletonCreation() {
         assertEquals(api, PlayerAPI.PLAYER_API);
         assertNotNull(api);
+    }
+
+    @Test
+    public void testSetCameraInputSizeIntInt() {
+        api.setCameraInputSize(width, height);
+        assertEquals(new Vector2(width, height), api.getCameraInputSize());
+    }
+
+    @Test
+    public void testSetCameraInputSizeVector2() {
+        Vector2 inputSize = new Vector2(width, height);
+        api.setCameraInputSize(inputSize);
+        assertEquals(inputSize, api.getCameraInputSize());
+        assertNotSame(inputSize, api.getCameraInputSize());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetCameraInputSizeNull() {
+        api.setCameraInputSize(null);
     }
 
     @Test
