afb4e7de9f24e28f8559fd03a75406de72afa551
LiamClark <L.A.Clark@student.tudelft.nl>
2015-06-11 10:52:49 +0200

    added naive approach for tracking the player

--- core/src/main/java/nl/tudelft/contextproject/core/playertracking/PlayerTracker.java
+++ core/src/main/java/nl/tudelft/contextproject/core/playertracking/PlayerTracker.java
@@ -1,6 +1,8 @@
 package nl.tudelft.contextproject.core.playertracking;
 
 import com.badlogic.gdx.math.Vector2;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import nl.tudelft.contextproject.core.entities.ColourPalette;
 import nl.tudelft.contextproject.core.entities.Player;
 import nl.tudelft.contextproject.core.input.PlayerPosition;
@@ -8,9 +10,11 @@ import nl.tudelft.contextproject.core.input.PlayerPosition;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class PlayerTracker {
+    private static final float MAX_DISTANCE = 300;
     protected List<Player> playerList = new ArrayList<>();
 
     /**
@@ -33,9 +37,32 @@ public class PlayerTracker {
      * @return The Player
      */
     public Player trackPlayer(PlayerPosition playerPosition) {
-        // TODO: Actually track players instead of using every position
+        Optional<Pair> playerDistPair = playerList.parallelStream()
+                .map(p -> new Pair(p, distance(p, playerPosition)))
+                .filter(pair -> pair.getDist() < MAX_DISTANCE)
+                .min(Pair::compareTo);
+
+
         Vector2 center = playerPosition.getCenterOfPlayer();
         return new Player(ColourPalette.standardPalette(), center.x, center.y,
                 playerPosition.getRadiusOfCircle());
     }
+
+    public static float distance(Player p, PlayerPosition playerPosition) {
+        return p.getPosition().dst(playerPosition.getCenterOfPlayer());
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Pair implements Comparable<Pair> {
+        Player p;
+
+        float dist;
+
+        @Override
+        public int compareTo(Pair o) {
+            return Float.compare(this.dist,o.dist);
+        }
+    }
+
 }
